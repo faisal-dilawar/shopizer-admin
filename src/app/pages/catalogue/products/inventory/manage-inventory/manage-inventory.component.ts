b36ce2566f2966aa07e756d7e50e5871399a87b5
Faisal Dilawar <faisal@technogise.com>
2026-02-26 14:07:53 +0530

    Stage 3: Adding CI Also fixedthe stack overflow issue

--- src/app/pages/catalogue/products/inventory/manage-inventory/manage-inventory.component.ts
+++ src/app/pages/catalogue/products/inventory/manage-inventory/manage-inventory.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { LocalDataSource } from 'ng2-smart-table';
 import { ProductService } from '../../services/product.service';
